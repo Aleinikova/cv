c81be916139621c6c7b529c5a0ee7f05780e65f6
Valeria Mahilevich <aleinikavalera@gmail.com>
2023-11-14 21:56:41 +0100

    implement footer

--- app/(homepage)/(components)/Footer.tsx
+++ app/(homepage)/(components)/Footer.tsx
@@ -1,5 +1,60 @@
 function FooterEl() {
-  return <footer className='bg-text-primary px-28 py-40'>123</footer>;
+  return (
+    <footer className='flex flex-col items-center bg-text-primary px-28 pb-9 pt-40'>
+      {/* TOP BUTTONS ROW */}
+      <div className='flex w-3/4 items-center justify-between'>
+        <div className='flex w-auto flex-col items-center'>
+          <div className='h-24 w-24 rounded-full bg-brand-yellow' />
+          <div className='mt-2 text-white'>ON/OFF</div>
+        </div>
+        <div className='flex w-auto flex-col items-center'>
+          <div className='h-24 w-24 rounded-full bg-brand-yellow' />
+          <div className='mt-2 text-white'>S/P</div>
+        </div>
+        <div className='flex w-auto flex-col items-center'>
+          <div className='h-24 w-24 rounded-full bg-brand-yellow' />
+          <div className='mt-2 text-white'>SOUND</div>
+        </div>
+        <div className='flex w-auto flex-col items-center'>
+          <div className='h-24 w-24 rounded-full bg-brand-yellow' />
+          <div className='mt-2 text-white'>SOUND</div>
+        </div>
+      </div>
+      {/* SECOND BUTTONS ROW */}
+      <div className='mt-20 flex w-full items-center justify-between'>
+        {/* MAIN CONTROL */}
+        <div>
+          <div className='relative flex w-auto flex-col items-center'>
+            <div className='h-40 w-40 rounded-full bg-brand-yellow' />
+            <div className='absolute -bottom-[28px] text-white'>TOP</div>
+          </div>
+          <div className='flex'>
+            <div className='relative mr-40 flex w-auto flex-col items-center'>
+              <div className='h-40 w-40 rounded-full bg-brand-yellow' />
+              <div className='absolute -bottom-[28px] text-white'>LEFT</div>
+            </div>
+            <div className='relative flex w-auto flex-col items-center'>
+              <div className='h-40 w-40 rounded-full bg-brand-yellow' />
+              <div className='absolute -bottom-[28px] text-white'>RIGHT</div>
+            </div>
+          </div>
+          <div className='relative flex w-auto flex-col items-center'>
+            <div className='h-40 w-40 rounded-full bg-brand-yellow' />
+            <div className='absolute -bottom-[28px] text-white'>BOTTOM</div>
+          </div>
+        </div>
+        {/* ROTATE BUTTON */}
+        <div className='relative flex w-auto flex-col items-center'>
+          <div className='h-52 w-52 rounded-full bg-brand-yellow' />
+          <div className=' mt-2 text-white'>ROTATE</div>
+        </div>
+      </div>
+
+      <div className='mt-40 font-secondary text-white'>
+        designed by Valeria Mahilevich
+      </div>
+    </footer>
+  );
 }
 
 export default FooterEl;
